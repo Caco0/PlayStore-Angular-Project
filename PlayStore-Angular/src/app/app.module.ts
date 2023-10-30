896c100583c45a9f5a782e33707a68616e78c1e5
Rafael Dametto <cacotpds@gmail.com>
2023-10-30 15:35:09 -0300

    Initial styling of the nav__bar menu

--- PlayStore-Angular/src/app/app.module.ts
+++ PlayStore-Angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
