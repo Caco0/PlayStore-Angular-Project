4a1a7e6fc4a042f9ea2895e1043eef268be24bd2
Rafael Dametto <cacotpds@gmail.com>
2023-11-13 16:29:35 -0300

    components banner card label and pricing + styling

--- PlayStore-Angular/src/app/app.module.ts
+++ PlayStore-Angular/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPricingComponent } from './components/card/card-pricing/card-pricing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardComponent,
-    NavBarComponent
+    NavBarComponent,
+    BannerComponent,
+    CardLabelComponent,
+    CardPricingComponent
   ],
   imports: [
     BrowserModule,
---
+++ PlayStore-Angular/src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css'],
+})
+export class CardPricingComponent implements OnInit {
+  @Input()
+  gameMedia: string = '';
+  @Input()
+  gamePrice: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
