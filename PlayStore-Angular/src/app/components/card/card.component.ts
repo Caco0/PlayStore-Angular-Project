10c75dddb7bd8ef448fea26c03a714c9a08e0e1b
Rafael Dametto <cacotpds@gmail.com>
2023-11-22 14:32:35 -0300

    Added parent cards, more child properties

--- PlayStore-Angular/src/app/components/card/card.component.ts
+++ PlayStore-Angular/src/app/components/card/card.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent implements OnInit {
+  @Input()
+  gameImage: string = '';
+  @Input()
+  gameLabel: string = '';
+  @Input()
+  gameMedia: string = 'Media digital PS5';
+  @Input()
+  gamePrice: string = 'R$ 249,99';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
